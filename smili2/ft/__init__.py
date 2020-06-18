076ea4f8dd9dc472c01da34cca2e266b01de9f40
Kazu Akiyama <kazunori.akiyama.kazu@gmail.com>
2020-06-18 17:25:22 -0400

    minor edit on format; no net change

---
+++ smili2/ft/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+This module wrap up functions related to Fourier Tranformations
+'''
+__author__ = "Smili Developer Team"
+
+from .ft_image import NFFT_Image
