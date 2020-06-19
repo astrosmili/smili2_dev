e0b4c1c41337807d31a10d6058b760d72ecafe8d
Kazu Akiyama <kazunori.akiyama.kazu@gmail.com>
2020-06-18 23:19:22 -0400

    format modification; no net change

--- smili2/util/gp.py
+++ smili2/util/gp.py
@@ -8,20 +8,29 @@ from sklearn import gaussian_process
 from sklearn.gaussian_process.kernels import ConstantKernel, RBF, WhiteKernel
 
 
-def gp_interp(xin, yin, sigmain=None, xpred=None, length_scale=None, fit_noise=True, n_restarts_optimizer=1, return_std=False, return_gp=False, **gprargs):
+def gp_interp(xin, yin, sigmain=None, xpred=None, length_scale=None, fit_noise=True,
+              n_restarts_optimizer=1, return_std=False, return_gp=False, **gprargs):
     '''
     Interpolation / Smoothing with Gaussian processing using an RBF kernel.
     This function will use the Gaussian Process Regressor in scikit-learn.
 
     Args:
-        xin, yin (1d array-like) : input signal
-        sigmain (optional, 1d array-like) : 1 sigma uncertainty of yin
-        xpred (optional, 1d array-like) : x-axis value of the output signal. If not specified, xin will be used.
-        length_scale (optional, float) : a length scale of the RBF kernel to be used. The default value is the minimum interval of xin.
-        fit_noise (optional, boolean) : If true, the global noise will be fitted as well.
-        n_restarts_optimizer (optional, integer) : GP fit will be repeated for this value to avoid local minima.
-        return_std (optional, boolean) : If true, the uncertainty of ypred will be output.
-        return_gp (optional, boolean) : If true, the instance of the Gaussian Process Regressor will be returned.
+        xin, yin (1d array-like) :
+            input signal
+        sigmain (optional, 1d array-like) :
+            1 sigma uncertainty of yin
+        xpred (optional, 1d array-like) :
+            x-axis value of the output signal. If not specified, xin will be used.
+        length_scale (optional, float) :
+            a length scale of the RBF kernel to be used. The default value is the minimum interval of xin.
+        fit_noise (optional, boolean) :
+            If true, the global noise will be fitted as well.
+        n_restarts_optimizer (optional, integer) :
+            GP fit will be repeated for this value to avoid local minima.
+        return_std (optional, boolean) :
+            If true, the uncertainty of ypred will be output.
+        return_gp (optional, boolean) :
+            If true, the instance of the Gaussian Process Regressor will be returned.
     Returns:
         ypred: predicted y-value on xpred.
         sigmapred (optional; if return_std==True): sigma estimators on ypred
