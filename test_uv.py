51f456a9c31c9e607e73107be8283d17dbea11e4
Leonid Benkevitch <benkev@mit.edu>
2021-04-14 20:55:09 -0400

    ...

---
+++ test_uv.py
@@ -0,0 +1,12 @@
+import smili2.uvdata as uv
+
+# uv.load_uvfits('/home/benkev/ALMA/alma/alma.alma.cycle7.10_noise.uvfits',
+#                '/home/benkev/ALMA/alma_zarr')
+
+uv.load_uvfits('/home/benkev/ALMA/3C273.coeff.fittp.uvfits',
+               '/home/benkev/ALMA/3C273_zarr')
+
+
+
+
+
