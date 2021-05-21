e9d233921feefa09cb5e70f98e44b5aea7fe52c6
Leonid Benkevitch <benkev@mit.edu>
2021-05-21 03:54:15 -0400

    ...

--- test_uv.py
+++ test_uv.py
@@ -1,3 +1,4 @@
+import numpy as np
 import smili2.uvdata as uv
 
 # uvd= uv.load_uvfits('/home/benkev/ALMA/alma/alma.alma.cycle7.10_noise.uvfits',
@@ -9,6 +10,9 @@ uvd = uv.load_uvfits('/home/benkev/ALMA/3C273.coeff.fittp.uvfits',
 ds = uvd.vistab.ds
 vs = ds.vis
 
-#vst = uv.switch_polrepr(vs, 'stokes')
 
+vsar = vs.data.compute()   # Extract visibility ndarray
+shape1 = list(vsar.shape)
+shape1[-1] = 4             # Extend pol axis to 4 to hold 4 Stokes parameters
+vsar1 = np.zeros(shape1)   # New visibility ndarray for Stokes parameters
 
