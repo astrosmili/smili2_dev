b5340fbf2eae5bdec133132eb30a27ab299eb5c4
Leonid Benkevitch <lbenkev@gmail.com>
2021-08-09 18:41:47 -0400

    ...

--- test_uv.py
+++ test_uv.py
@@ -30,8 +30,11 @@ polid2name = { # Possible values of CRVAL3 in .uvfits headers
 # in_uvfits = '/home/benkev/ALMA/J2203TSTCL5_SUBSP_avch.uvfits'
 # out_zarr =  '/home/benkev/ALMA/J2203TSTCL5_SUBSP_avch_zarr'
 
-in_uvfits = '/data-smili/J2203TSTCL5_SUBSP_avch.uvfits'
-out_zarr =  '/data-smili/J2203TSTCL5_SUBSP_avch_zarr'
+# in_uvfits = '/data-smili/J2203TSTCL5_SUBSP_avch.uvfits'
+# out_zarr =  '/data-smili/J2203TSTCL5_SUBSP_avch_zarr'
+
+in_uvfits = '/data-smili/J2203TSTCL5_SUBSP.uvfits'
+out_zarr =  '/data-smili/J2203TSTCL5_SUBSP_zarr'
 
 pseudoI = False
 
